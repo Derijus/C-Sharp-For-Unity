b66aa1e30614db1060b78b47822a98b2b6463750
Derijus <86003402+Derijus@users.noreply.github.com>
2022-09-20 14:16:43 +0200

    Add files via upload

---
+++ Calculator/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welkom bij de C#-Calculator.");
+            Console.WriteLine("Geef een getal.");
+            double getal1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Optellen(typ +), aftrekken(typ -), vermenigvuldigen(typ X), of delen(typ /)?");
+            string operat = Console.ReadLine();
+            Console.WriteLine("Geef het tweede getal.");
+            double getal2 = Convert.ToInt32(Console.ReadLine());
+            double uitkomst = 0;
+            if (operat == "+")
+            {
+                uitkomst = getal1 + getal2;
+            }
+            else if (operat == "-")
+            {
+                uitkomst = getal1 - getal2;
+            }
+            else if (operat == "X")
+            {
+                uitkomst = getal1 * getal2;
+            }
+            else if (operat == "/")
+            {
+                if (getal2 != 0)
+                {
+                    uitkomst = getal1 / getal2;
+                }
+                else
+                {
+                    Console.WriteLine("Je kan niet delen door 0.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Dit is geen geldige invoer.");
+            }
+            Console.WriteLine("De uitkomst is " + uitkomst);
+        }
+    }
+}
---
+++ Guess game/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Guess_game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welkom bij de Guess Game.");
+            for (int pogingen = 3; pogingen != 0; pogingen--)
+            {
+                Console.WriteLine("Raad het woord. Je hebt nog " + pogingen + " pogingen. (Tip: het verloor een race tegen een schildpad)");
+                string raden = Console.ReadLine();
+                if (raden.ToLower() == "haas")
+                {
+                    Console.WriteLine("Je hebt het woord geraden!");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Fout!");
+                }
+            }
+        }
+    }
+}
