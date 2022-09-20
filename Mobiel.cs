f73364f3319afb3073c126ca9075db87e61c484e
Derijus <86003402+Derijus@users.noreply.github.com>
2022-09-20 14:12:10 +0200

    Add files via upload

---
+++ Mobiel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MobielShop
+{
+    public class Mobiel
+    {
+        public string Type;
+        public string Model;
+        public string OS;
+        public string CEO;
+        public double Prijs;
+        public Mobiel(string nType, string nModel, string nOS, string nCEO, double nPrijs)
+        {
+            Type = nType;
+            Model = nModel;
+            OS = nOS;
+            CEO = nCEO;
+            Prijs = nPrijs;
+        }
+    }
+}
